a15c1fd9bfee1a1b37d515be60ef4c82f9d8c7fb
jeevams <58686474+jeevams@users.noreply.github.com>
2020-08-23 08:50:21 +0530

    Update Login_Stu.java

--- Login_Stu.java
+++ Login_Stu.java
@@ -17,49 +17,47 @@ public class Login_Stu
   EditText mail;
   EditText pass;
   
-  protected void onCreate(Bundle paramBundle)
+  protected void onCreate(Bundle savedInstanceState)
   {
-    super.onCreate(paramBundle);
-    requestWindowFeature(1);
-    setContentView(2131427358);
-    this.db = new Data(this);
-    this.mail = ((EditText)findViewById(2131230788));
-    this.pass = ((EditText)findViewById(2131230848));
-    this.btlogin = ((Button)findViewById(2131230823));
-    this.btlogin.setOnClickListener(new View.OnClickListener()
+    super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    setContentView(R.layout.activity_login__stu);
+    db = new Data(this);
+    mail = ((EditText)findViewById(email_login));
+    pass = ((EditText)findViewById(password_login));
+    btlogin = ((Button)findViewById(login_stu));
+    btlogin.setOnClickListener(new View.OnClickListener()
     {
-      public void onClick(View paramAnonymousView)
+      public void onClick(View v)
       {
-        String str1 = Login_Stu.this.mail.getText().toString();
-        String str2 = Login_Stu.this.pass.getText().toString();
-        boolean bool = Login_Stu.this.db.checkmaila(str1, str2);
+        String str1 = mail.getText().toString();
+        String str2 = pass.getText().toString();
+        boolean bool = db.checkmaila(str1, str2);
         if ((!str1.equals("")) && (!str2.equals("")))
         {
           if ((str1.equals("jeevams")) && (str2.equals("pulsar150")))
           {
-            Toast.makeText(Login_Stu.this.getApplicationContext(), "Login Successfull", 1).show();
-            Intent localIntent2 = new Intent(Login_Stu.this, Query_View.class);
-            Login_Stu.this.startActivity(localIntent2);
+            Toast.makeText(getApplicationContext(), "Login Successfull",Toast.LENGTH_SHORT).show();
+           
+            Intent intent = new Intent(Login_Stu.this, Query_View.class);
+            startActivity(intent);
             return;
           }
           if (!bool)
           {
-            Toast.makeText(Login_Stu.this.getApplicationContext(), "Login Successfull", 1).show();
-            Intent localIntent1 = new Intent(Login_Stu.this, Query_Post.class);
-            Login_Stu.this.startActivity(localIntent1);
+            Toast.makeText(getApplicationContext(), "Login Successfull",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Login_Stu.this, Query_Post.class);
+            startActivity(intent);
             return;
           }
-          Toast.makeText(Login_Stu.this.getApplicationContext(), "incorrect user name and password", 1).show();
+          Toast.makeText(getApplicationContext(), "incorrect user name and password",Toast.LENGTH_SHORT).show();
           return;
         }
-        Toast.makeText(Login_Stu.this.getApplicationContext(), "Field is empty", 1).show();
+        Toast.makeText(getApplicationContext(), "Field is empty",Toast.LENGTH_SHORT).show();
       }
     });
   }
 }
 
-
-/* Location:           C:\MAD\MadApp_dex2jar.jar
- * Qualified Name:     com.example.programmingknowledge.madapp.Login_Stu
- * JD-Core Version:    0.7.0.1
- */
+
+
